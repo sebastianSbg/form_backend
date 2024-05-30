9e27e342ea8a0b3c0996cb09072dc35b62ae5fdd
Sebastian Bommer <sebastan.bommer.sbg@gmail.com>
2024-05-29 16:48:02 -0800

    updated with fields

---
+++ savetodb/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+
+
+class Product(models.Model):
+    # New fields from JSON
+    stay_valid = models.BooleanField(default=False)
+    stay_arrival_date = models.DateTimeField(null=True, blank=True)
+    stay_departure_date = models.DateTimeField(null=True, blank=True)
+
+    # Assuming a max of 5 persons based on provided JSON structure
+    person_first_name_0 = models.CharField(max_length=255, blank=True)
+    person_last_name_0 = models.CharField(max_length=255, blank=True)
+    person_country_0 = models.CharField(max_length=255, blank=True)
+    person_sex_0 = models.CharField(max_length=10, blank=True)
+    person_birth_date_0 = models.DateTimeField(null=True, blank=True)
+
+    person_first_name_1 = models.CharField(max_length=255, blank=True)
+    person_last_name_1 = models.CharField(max_length=255, blank=True)
+    person_country_1 = models.CharField(max_length=255, blank=True)
+    person_sex_1 = models.CharField(max_length=10, blank=True)
+    person_birth_date_1 = models.DateTimeField(null=True, blank=True)
+
+    person_first_name_2 = models.CharField(max_length=255, blank=True)
+    person_last_name_2 = models.CharField(max_length=255, blank=True)
+    person_country_2 = models.CharField(max_length=255, blank=True)
+    person_sex_2 = models.CharField(max_length=10, blank=True)
+    person_birth_date_2 = models.DateTimeField(null=True, blank=True)
+
+    person_first_name_3 = models.CharField(max_length=255, blank=True)
+    person_last_name_3 = models.CharField(max_length=255, blank=True)
+    person_country_3 = models.CharField(max_length=255, blank=True)
+    person_sex_3 = models.CharField(max_length=10, blank=True)
+    person_birth_date_3 = models.DateTimeField(null=True, blank=True)
+
+    person_first_name_4 = models.CharField(max_length=255, blank=True)
+    person_last_name_4 = models.CharField(max_length=255, blank=True)
+    person_country_4 = models.CharField(max_length=255, blank=True)
+    person_sex_4 = models.CharField(max_length=10, blank=True)
+    person_birth_date_4 = models.DateTimeField(null=True, blank=True)
+
+    addr_street = models.CharField(max_length=255, blank=True)
+    addr_valid = models.BooleanField(default=False)
+    addr_city = models.CharField(max_length=255, blank=True)
+    addr_zip = models.CharField(max_length=20, blank=True)
+    addr_country = models.CharField(max_length=255, blank=True)
+
+    id_number = models.CharField(max_length=255, blank=True)
+    id_valid = models.BooleanField(default=False)
+    id_issue_date = models.DateTimeField(null=True, blank=True)
+    id_institution = models.CharField(max_length=255, blank=True)
+    id_country = models.CharField(max_length=255, blank=True)
+
+    signature = models.TextField(blank=True)
---
+++ savetodb/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Product
+from .serializers import ProductSerializer
+
+from rest_framework import status
+
+# Create your views here.
+
+@api_view(['GET', 'POST'])
+def product_list(request):
+    if request.method == 'GET':
+        return Response('STORE GET REQUEST')
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        print(serializer.validated_data)  # returns a dictionary of the data
+        Product(**serializer.validated_data).save()
+        return Response('submitted')
+
+
+@api_view()
+def productDetail(request, id):
+    try:
+        product = Product.objects.get(pk=id)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    except Product.DoesNotExist:
+        return Response(status=404)
