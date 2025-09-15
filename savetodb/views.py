1c9e987b31fd2fbcadd5e5f2579170b05a1e8345
Sebastian Bommer <sebastan.bommer.sbg@gmail.com>
2025-09-14 22:54:49 -0700

    not sending mail if fail

--- savetodb/views.py
+++ savetodb/views.py
@@ -143,10 +143,10 @@ def product_list(request):
         try:
             form_template = Path('savetodb/static/form_template.pdf')
             pdf_out = fill_guest_registration_pdf(serializer_data, form_template, dict_map)
-            # send_email_with_attachment(pdf_out)
+            send_email_with_attachment(pdf_out) #THIS MIGHT FAIL AT TIMES
             os.remove(pdf_out)
         except Exception as e:
-            # send_form_failed_email()
+            # send_form_failed_email() #THIS MIGHT FAIL AT TIMES
             print("Couldn't send email.")
             print(e)
 
